2d4ad1d2c49eb2618bdb55e72469ea1fe5462973
NindyaShafira24 <nindyashafira585@gmail.com>
2025-02-18 15:40:57 +0700

    View controler

--- routes/web.php
+++ routes/web.php
@@ -71,3 +71,10 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
+
+
+Route::get('/greeting', function () {
+	return view('hello', ['name' => 'Nindya']);
+});
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
